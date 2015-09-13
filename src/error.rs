73a989d727c551594e5e351b8f91bca41744ba4d
Juan Ibiapina <juanibiapina@gmail.com>
2015-09-13 19:47:13 -0300

    refactor: Improve error handling

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,7 @@
 use std::error::Error as StdError;
 
 pub enum Error {
+    EndOfInput,
+    EmptyCommand,
     CommandNotFound(String),
 }
--- src/repl.rs
+++ src/repl.rs
@@ -3,33 +3,45 @@ use readline;
 use error::Error;
 use command_line::CommandLine;
 
-fn process(input: String) {
+fn process(input: String) -> Result<(), Error> {
     let command_line = CommandLine::parse(input);
 
-    match command_line.run() {
-        Ok(()) => {},
-        Err(Error::CommandNotFound(command)) => {
-            println!("lish: command not found: {}", command)
-        },
+    try!(command_line.run());
+
+    Ok(())
+}
+
+fn rep() -> Result<(), Error> {
+    let input = match readline::readline(":) ") {
+        Some(input) => input,
+        None => return Err(Error::EndOfInput),
     };
+
+    let trimmed_input = input.trim();
+
+    if trimmed_input.len() == 0 {
+        return Err(Error::EmptyCommand);
+    }
+
+    try!(process(trimmed_input.to_string()));
+
+    Ok(())
 }
 
 pub fn run() {
     loop {
-        let input = match readline::readline(":) ") {
-            Some(input) => input,
-            None => {
+        match rep() {
+            Ok(()) => {},
+            Err(Error::EndOfInput) => {
                 println!("");
                 break;
             },
+            Err(Error::EmptyCommand) => {
+                continue;
+            },
+            Err(Error::CommandNotFound(command)) => {
+                println!("lish: command not found: {}", command)
+            },
         };
-
-        let trimmed_input = input.trim();
-
-        if trimmed_input.len() == 0 {
-            continue
-        }
-
-        process(trimmed_input.to_string());
     }
 }
