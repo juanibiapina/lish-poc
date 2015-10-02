5e61065d31570cceb65f7cdc1de7510c2d5376cd
Juan Ibiapina <juanibiapina@gmail.com>
2015-10-01 22:50:34 -0300

    refactor: Add abstraction for list types when evaluating

--- src/lisp/eval.rs
+++ src/lisp/eval.rs
@@ -1,25 +1,43 @@
 use lisp::types;
 use lisp::env::{Env, env_new, env_get, env_set};
 
+enum FormType {
+    Def,
+    Let,
+    Function,
+}
+
+impl FormType {
+    pub fn from(name: &str) -> FormType {
+        match name {
+            "def!" => FormType::Def,
+            "let*" => FormType::Let,
+            _ => FormType::Function,
+        }
+    }
+}
+
 pub fn eval(ast: types::LispValue, env: Env) -> types::LispResult {
     match *ast {
-        types::LispType::List(_) => (),
+        types::LispType::List(_) => return eval_list(ast, env),
         _ => return eval_ast(ast, env),
     };
+}
 
-    let (head, elements) = match *ast {
+pub fn eval_list(ast: types::LispValue, env: Env) -> types::LispResult {
+    let (form_type, elements) = match *ast {
         types::LispType::List(ref elements) => {
             let ref head = *elements[0];
             match *head {
-                types::LispType::Symbol(ref name) => (name.to_string(), elements),
-                _ => ("__<fn*>__".to_string(), elements),
+                types::LispType::Symbol(ref name) => (FormType::from(name), elements),
+                _ => (FormType::Function, elements),
             }
         },
         _ => panic!("unreachable code"),
     };
 
-    match head.as_ref() {
-        "def!" => {
+    match form_type {
+        FormType::Def => {
             let a1 = (*elements)[1].clone();
             let a2 = (*elements)[2].clone();
             let r = try!(eval(a2, env.clone()));
@@ -31,7 +49,7 @@ pub fn eval(ast: types::LispValue, env: Env) -> types::LispResult {
                 _ => panic!("def! of non-symbol"),
             }
         },
-        "let*" => {
+        FormType::Let => {
             let let_env = env_new(Some(env.clone()));
             let a1 = (*elements)[1].clone();
             let a2 = (*elements)[2].clone();
@@ -54,7 +72,7 @@ pub fn eval(ast: types::LispValue, env: Env) -> types::LispResult {
             }
             return eval(a2, let_env.clone());
         },
-        _ => {
+        FormType::Function => {
             let el = try!(eval_ast(ast.clone(), env.clone()));
             let args = match *el {
                 types::LispType::List(ref args) => args,
